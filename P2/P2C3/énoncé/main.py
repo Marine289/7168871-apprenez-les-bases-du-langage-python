68ed01de2d81e832b8daffb3675e51c4a67742d0
Marine289 <marineleparquier@gmail.com>
2025-03-29 14:19:35 +0100

    Update main.py

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,1 +1,10 @@
-# Ecrivez votre code ici
+ def salaire_mensuel(salaire_annuel):
+...     resultat = salaire_annuel / 12
+...     return resultat
+def salaire_hebdomadaire(salaire_mensuel):
+...     resultat = salaire_mensuel / 4
+...     return resultat
+def salaire_horaire(salaire_hebdomadaire,heures_travaillees):
+...     resultat = salaire_hebdomadaire / heures_travaillees
+...     return resultat
+
